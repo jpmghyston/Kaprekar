9611f8e8d403b49ee4260f4f6817ef0b8506c2b6
Jake Mckenna <Jake.Mckenna@softwire.com>
2014-09-05 16:16:46 +0100

    Initial commit

---
+++ Kaprekar.Tests/KaprekarHelperTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Kaprekar.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class KaprekarHelperTests
+    {
+        private KaprekarHelper helper;
+
+        [SetUp]
+        public void SetUp() {}
+
+        [Test]
+        public void Kaprekar_helper_returns_blah()
+        {
+            //Given
+        }
+    }
+}
